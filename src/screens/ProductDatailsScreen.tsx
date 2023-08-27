5d78a9d3519e22725f3f4beaaeb68b5975de5560
Santos-Samuels <saamsmith15@gmail.com>
2023-08-26 22:54:23 -0300

    ♻️ refactor: add product datail screen content

--- src/screens/ProductDatailsScreen.tsx
+++ src/screens/ProductDatailsScreen.tsx
@@ -1,16 +1,74 @@
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { ScreenContainer } from '@src/components';
-import { RootStackParamList } from '@src/routes/stack.routes';
-import { Text } from 'react-native';
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { Button, ProductRate, ScreenContainer } from "@src/components";
+import { RootStackParamList } from "@src/routes/stack.routes";
+import { getOfferPrice } from "@src/shared/helpers/getOfferPrice";
+import { ProductServices } from "@src/shared/services";
+import { useQuery } from "@tanstack/react-query";
+import { Image, Text, View } from "react-native";
 
 type ScreenProps = NativeStackScreenProps<RootStackParamList, "ProductDatails">;
 
 const ProductDatailsScreen: React.FC<ScreenProps> = ({ route }) => {
-  return (
-    <ScreenContainer>
-      <Text>ProductDatailsScreen - {route.params.productId}</Text>
-    </ScreenContainer>
-  )
-}
+  const { productId } = route.params;
 
-export default ProductDatailsScreen
+  const { isLoading, data } = useQuery({
+    queryKey: [`product-${productId}`],
+    queryFn: ProductServices.getById.bind(null, productId),
+  });
+
+  const formatDescription = (description: string) => {
+    return description[0].toUpperCase() + description.substring(1);
+  };
+
+  if (isLoading) return <Text>Loading...</Text>;
+
+  const product = data?.data!;
+
+  return (
+    <ScreenContainer justifyContent="justify-start">
+      <View className="bg-white -mt-16 rounded-b-3xl w-screen justify-self-start">
+        <Image
+          className="self-center mb-2 mt-24"
+          source={{ uri: product.image }}
+          style={{ height: 350, width: 350 }}
+          resizeMode="contain"
+        />
+
+        <Text className="text-center mb-3 text-zinc-500">
+          {product.category}
+        </Text>
+      </View>
+
+      <View className="m-3 flex-1 justify-between">
+        <View>
+          <Text className="text-lg font-semibold">{product.title}</Text>
+
+          <Text className="text-lg font-semibold mt-3">Description</Text>
+          <Text className="text-zinc-600 mb-3">
+            {formatDescription(product.description)}
+          </Text>
+
+          <ProductRate rating={product.rating} starsSize={25} key={`rate-product-${product.id}`} />
+        </View>
+
+        <View className="flex-row items-end justify-between">
+          <View>
+            <Text className="text-lg font-semibold mt-3">Price</Text>
+            <View className="flex-row items-end gap-3">
+              <Text className="font-bold text-3xl text-amber-500 -mb-1">
+                R$ {getOfferPrice(product.price)}
+              </Text>
+              <Text className="text-normal text-zinc-400 line-through">
+                R$ {product.price}
+              </Text>
+            </View>
+          </View>
+
+          <Button title="Add to Cart" onPress={() => {}} />
+        </View>
+      </View>
+    </ScreenContainer>
+  );
+};
+
+export default ProductDatailsScreen;
