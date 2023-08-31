8543e73f3bd3c110ca7b033e1ef6890eb6c4a53e
Santos-Samuels <saamsmith15@gmail.com>
2023-08-31 09:25:03 -0300

    ✅ test: update cart item tests

--- src/components/molecules/__test__/CartItem.spec.tsx
+++ src/components/molecules/__test__/CartItem.spec.tsx
@@ -1,4 +1,5 @@
-import { render } from "@testing-library/react-native";
+import { AppProvider } from "@src/context/AppContext";
+import { fireEvent, render } from "@testing-library/react-native";
 import { cartItem } from "../../../../test/cart.mock";
 import CartItem from "../CartItem";
 
@@ -31,5 +32,63 @@ describe("CartItem", () => {
 
     expect(counter).toBeTruthy();
     expect(counter.props["aria-valuenow"]).toBe(cartItem.quantity);
+  });
+
+  it("should be able to increase the item quantity by 1", () => {
+    const { getByTestId } = render(<CartItem item={cartItem} />, {
+      wrapper: AppProvider,
+    });
+
+    const counter = getByTestId("counter");
+    const increaseButton = getByTestId("increase");
+
+    expect(counter.props["aria-valuenow"]).toBe(cartItem.quantity);
+    fireEvent.press(increaseButton);
+
+    expect(counter.props["aria-valuenow"]).toBe(cartItem.quantity++);
+  });
+
+  it("should not be able to increase the item quantity by 1 if the quantity is equal to 10", () => {
+    cartItem.quantity = 10;
+    const { getByTestId } = render(<CartItem item={cartItem} />, {
+      wrapper: AppProvider,
+    });
+
+    const counter = getByTestId("counter");
+    const increaseButton = getByTestId("increase");
+
+    expect(counter.props["aria-valuenow"]).toBe(10);
+    fireEvent.press(increaseButton);
+
+    expect(counter.props["aria-valuenow"]).toBe(10);
+  });
+
+  it("should be able to decrease the item quantity by 1", () => {
+    const { getByTestId } = render(<CartItem item={cartItem} />, {
+      wrapper: AppProvider,
+    });
+
+    const counter = getByTestId("counter");
+    const decreaseButton = getByTestId("decrease");
+
+    expect(counter.props["aria-valuenow"]).toBe(cartItem.quantity);
+    fireEvent.press(decreaseButton);
+
+    expect(counter.props["aria-valuenow"]).toBe(cartItem.quantity--);
+  });
+
+  it("should not be able to decrease the item quantity by 1 if the quantity is equal to 1", () => {
+    cartItem.quantity = 1;
+    const { getByTestId } = render(<CartItem item={cartItem} />, {
+      wrapper: AppProvider,
+    });
+
+    const counter = getByTestId("counter");
+    const decreaseButton = getByTestId("decrease");
+
+    expect(counter.props["aria-valuenow"]).toBe(1);
+    fireEvent.press(decreaseButton);
+
+    expect(counter.props["aria-valuenow"]).toBe(1);
   });
 });
